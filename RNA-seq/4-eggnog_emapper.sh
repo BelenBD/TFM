1c0d35c3cbb1afbd1cf587e91d6569c8f112b6c0
BelenBD <133240759+BelenBD@users.noreply.github.com>
2023-05-12 20:42:13 +0200

    Update 4-eggnog_emapper.sh

--- RNA-seq/4-eggnog_emapper.sh
+++ RNA-seq/4-eggnog_emapper.sh
@@ -6,19 +6,21 @@
 #SBATCH -o slurm/emapper.%j.out
 #SBATCH -e slurm/emapper.%j.err 
 
+# Download the necessary data for EggNOG mapper
 download_eggnog_data.py
 
+# Create a diamond database for the Fungi taxonomic group
 create_dbs.py -m diamond --dbname fungi --taxa Fungi
 
+# For each input FASTA file in the transabyss_merged_fasta directory, run EggNOG mapper with the diamond database and other options
 cd /mnt/lustre/home/belben01/transabyss_merged_fasta
-
 for file in $(ls | grep '.fa')
 	do
-		input=${file}
-		sample_name=${file%%.fa*}
-		folder=${sample_name}
-		mkdir /mnt/lustre/home/belben01/eggnogmapper/$folder
+		input=${file} 
+		sample_name=${file%%.fa*} 
+		folder=${sample_name} 
+		mkdir /mnt/lustre/home/belben01/eggnogmapper/$folder 
 		emapper.py -m diamond --itype CDS -i $input -o /mnt/lustre/home/belben01/eggnogmapper/$folder/$sample_name --dmnd_db /mnt/lustre/home/belben01/anaconda3/envs/eggnogmapper/lib/python3.7/site-packages/data/fungi.dmnd --tax_scope Saccharomycetaceae --decorate_gff yes --cpu 0
-
 	done
+
 	
