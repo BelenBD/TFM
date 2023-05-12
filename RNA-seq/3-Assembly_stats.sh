3da3bd601f23fa3d0210b06f2a624fbecbcfee4a
BelenBD <133240759+BelenBD@users.noreply.github.com>
2023-05-12 20:38:46 +0200

    Update 3-Assembly_stats.sh

--- RNA-seq/3-Assembly_stats.sh
+++ RNA-seq/3-Assembly_stats.sh
@@ -6,7 +6,7 @@
 #SBATCH -o slurm/stats.%j.out
 #SBATCH -e slurm/stats.%j.err
 
-# copio los merged fasta del transabyss en otra carpeta para hacer los assembly-stats
+# Copy merged fasta files from Transabyss to another directory for assembly-stats
 cd /mnt/lustre/home/belben01/transabyss-master/non-rRNA
 for folder in *
 	do
@@ -16,15 +16,22 @@ for folder in *
 			done
 	done
 
+# Change directory to the copied merged fasta files location
 cd /mnt/lustre/home/belben01/transabyss_merged_fasta
 
+# For each file with ".fa" extension in the current directory, run assembly-stats and save the output in a file with sample name in another directory
 for file in $(ls | grep '.fa')
 	do
 		sample_name=${file%%.fa*}
 		assembly-stats -t $file > /mnt/lustre/home/belben01/ass-stats/stats_$sample_name.txt
 	done
 
+# Store the header of the first document (it does not matter which one) from directory	
 file1=(*)
-head -1 $file1 > headers.txt #store the header of the first document (it does not matter which one) from directory	
-tail -q -n +2 *.txt > /mnt/lustre/home/belben01/ass-stats/merged_assembly.txt #merge all content from files removing headers (only picks second row)
-cat headers.txt merged_assembly.txt > final_assembly.txt #final table with all samples and headers
+head -1 $file1 > headers.txt
+
+# Merge all content from files removing headers (only picks second row) and save it to a file with merged assembly statistics
+tail -q -n +2 *.txt > /mnt/lustre/home/belben01/ass-stats/merged_assembly.txt
+
+# Merge the headers file and merged assembly statistics file to create a final table with all samples and headers
+cat headers.txt merged_assembly.txt > final_assembly.txt 
