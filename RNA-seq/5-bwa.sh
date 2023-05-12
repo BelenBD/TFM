7950eba1989189af795410576710ba046827a695
BelenBD <133240759+BelenBD@users.noreply.github.com>
2023-05-12 20:43:24 +0200

    Update 5-bwa.sh

--- RNA-seq/5-bwa.sh
+++ RNA-seq/5-bwa.sh
@@ -6,16 +6,37 @@
 #SBATCH -o slurm/bwa.%j.out
 #SBATCH -e slurm/bwa.%j.err 
 
-cd /mnt/lustre/home/belben01/eggnogmapper
+# Define input and output directories
+input_dir="/mnt/lustre/home/belben01/eggnogmapper"
+index_dir="/mnt/lustre/home/belben01/index-bwa"
+sam_dir="/mnt/lustre/home/belben01/bwa-sam"
 
-for folder in $(ls)
-	do
-		sample_name=${folder}
-		R1=${sample_name}_R1.fq.gz
-		R2=${sample_name}_R2.fq.gz
-		mkdir /mnt/lustre/home/belben01/index-bwa/$sample_name
-		bwa index -p /mnt/lustre/home/belben01/index-bwa/$sample_name/$sample_name -a is /mnt/lustre/home/belben01/STAR/Samples/$sample_name.fa
-		bwa mem /mnt/lustre/home/belben01/index-bwa/$sample_name/$sample_name /mnt/lustre/home/belben01/sortmerna/Reads/non-rRNA/$R1 /mnt/lustre/home/belben01/sortmerna/Reads/non-rRNA/$R2 > /mnt/lustre/home/belben01/bwa-sam/$sample_name.sam
+# Iterate over input directories
+for folder in "${input_dir}"/*
+do
+    if [[ -d "${folder}" ]]; then
+        sample_name="$(basename "${folder}")"
+        R1="${sample_name}_R1.fq.gz"
+        R2="${sample_name}_R2.fq.gz"
+        index_file="${index_dir}/${sample_name}/${sample_name}"
+        sam_file="${sam_dir}/${sample_name}.sam"
 
-	done
+        # Create index directory
+        mkdir -p "${index_dir}/${sample_name}"
+
+        # Index reference file with BWA
+        printf "Indexing reference file for %s...\n" "${sample_name}"
+        bwa index -p "${index_file}" -a is "${input_dir}/${sample_name}.fa"
+
+        # Map reads with BWA
+        printf "Mapping reads for %s...\n" "${sample_name}"
+        bwa mem "${index_file}" \
+            "${input_dir}/${R1}" \
+            "${input_dir}/${R2}" \
+            > "${sam_file}"
+    fi
+done
+
+echo "Done mapping reads with BWA."
+
 	
